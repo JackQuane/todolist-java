5feecb52c65bcb63ef7741ded099a7e1dba4c356
Jack Quane <jackquane1@gmail.com>
2021-04-13 10:10:44 +0100

    Login/Register

--- src/main/java/com/quane/todolist/dao/LoginDao.java
+++ src/main/java/com/quane/todolist/dao/LoginDao.java
@@ -1,4 +1,36 @@
 package com.quane.todolist.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.quane.todolist.model.LoginBean;
+import com.quane.todolist.utils.JDBCUtils;
+
+//JDBC code to validate login username and password with users table
+
 public class LoginDao {
+
+    public boolean validate(LoginBean loginBean) throws ClassNotFoundException {
+        boolean status = false;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (Connection connection = JDBCUtils.getConnection();
+            // Step 2:Create a statement using connection object
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from users where username = ? and password = ? ")) {
+            preparedStatement.setString(1, loginBean.getUsername());
+            preparedStatement.setString(2, loginBean.getPassword());
+
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            status = rs.next();
+
+        } catch (SQLException e) {
+            // process sql exception
+            JDBCUtils.printSQLException(e);
+        }
+        return status;
+    }
 }
